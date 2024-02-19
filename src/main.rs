2d703c29c995387d86a2b862eadfb7b0f72e3312
Jared Szymurski <jared@szymurski.com>
2024-02-19 14:37:28 -0500

    adding rest api stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+
+#[tokio::main]
+async fn main() {
+    // Define a route
+    let hello_route = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    // Start the Warp server
+    warp::serve(hello_route)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
